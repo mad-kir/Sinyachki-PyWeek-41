784da9d3eb6d53105b12c0140b1ab73fc6bfc5ad
Ekaterina Vinogradova <vinkata@yandex.ru>
2026-03-22 05:38:22 +0300

    добавлена базовая структура

--- game/game.py
+++ game/game.py
@@ -1,1 +1,33 @@
+import pygame
 
+pygame.init()
+
+
+def main():
+
+    fps = 60
+    clock = pygame.time.Clock()
+
+    screen_width, screen_height = 800, 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    
+    running = True
+    while running:
+        clock.tick(fps)
+        
+        #---ОБРАБОТКА СОБЫТИЙ---
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        #---ОБНОВЛЕНИЕ ИГРЫ---
+
+        #---ОТРИСОВКА---
+        
+        pygame.display.flip()
+    
+    pygame.quit()
+
+if __name__ == '__main__':
+    main()
