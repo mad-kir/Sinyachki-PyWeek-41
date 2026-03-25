72c35f4ea84006161b4575f7eb352a95eb853850
Ekaterina Vinogradova <vinkata@yandex.ru>
2026-03-25 09:12:06 +0300

    Пофиксила телепорт врага за пределами экрана: теперь он действительно может преследовать почти до самого конца уровня. Пофиксила баг с отображением окна смерти, добавила таймер, чтобы оно автоматически исчезало через 3 секунды. Добавила плейсхолдер 2 уровня и переход на него по маркеру

---
+++ game/classes/markers.py
@@ -0,0 +1,29 @@
+import pygame
+
+class Marker(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, image, type):
+        super().__init__()
+
+        self.x = x
+        self.y = y
+
+        self.width, self.height = width, height
+
+        self.image_surface = pygame.Surface((width, height))
+
+        self.image = image
+
+        self.rect = self.image_surface.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+
+        self.type = type
+
+        self.is_triggered = False
+
+    def update(self, screen, player, items, level):
+        
+        if self.rect.colliderect(player.rect):
+            self.is_triggered = True
+            print('triggered marker. is triggered = ', self.is_triggered)
--- game/windows/dead_window.py
+++ game/windows/dead_window.py
@@ -1,15 +1,27 @@
 import pygame
+import time
+
+from windows.quit_window import game_quit
 
 def game_over(screen, screen_width, screen_height):
     
-    
-    font = pygame.font.SysFont('fonts/RetroSans.ttf', 60)
-    text_surf = font.render('Вы мертвы.', True, (255, 0, 0))
-    text_rect = text_surf.get_rect()
-    text_rect.midtop = (screen_width/2, screen_height/2)
-    screen.blit(text_surf, text_rect)
+    begin = time.time()
+    return_to_game = False
+    while not return_to_game:
+        
+        screen.fill((0, 0, 0))
+        font = pygame.font.SysFont('fonts/RetroSans.ttf', 60)
+        text_surf = font.render('Вы мертвы.', True, (255, 0, 0))
+        text_rect = text_surf.get_rect()
+        text_rect.midtop = (screen_width/2, screen_height/2)
+        screen.blit(text_surf, text_rect)
 
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RETURN:
-                return True
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_quit(screen, screen_width, screen_height)
+
+        if time.time() - begin >= 3:
+            return_to_game = True
+            return True
